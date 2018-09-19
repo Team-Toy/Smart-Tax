01c9bab14f23b54ccc727e2e8f678c2d100450da
Bimal Chandra Debnath <bimal.debnath@northsouth.edu>
2018-09-20 02:31:01 +0600

    dataset updated again

--- Tax Calculator/Form2_Salaries.cs
+++ Tax Calculator/Form2_Salaries.cs
@@ -23,7 +23,7 @@ namespace Tax_Calculator
         private string[] data = {"false","true","0","0",
                                "false","true","0","0",
                                "false","true","0","0",
-                                "false","false","0.05","30000",
+                                "false","false","0.05","300000",
                                 "false","false","0.5","25000",
                                  "false","false","0.1","120000",";","1000000",
                                 "false","true","0","0",
